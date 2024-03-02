4262627fe0da554b01282652aaa3891b897b7f05
Ashok Choudhary <ashoka8769@gmail.com>
2024-03-02 14:11:18 +0530

    ADD: viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,7 +1,53 @@
 import React from "react";
+import styled from "styled-components";
 
 function Viewers() {
-  return <div>Viewers</div>;
+  return (
+    <Container>
+      <Wrap>
+        <img src="images/viewers-disney.png" alt="Viewrs" />
+      </Wrap>
+      <Wrap>
+        <img src="images/viewers-starwars.png" alt="Viewrs" />
+      </Wrap>
+      <Wrap>
+        <img src="images/viewers-marvel.png" alt="Viewrs" />
+      </Wrap>
+      <Wrap>
+        <img src="images/viewers-pixar.png" alt="Viewrs" />
+      </Wrap>
+      <Wrap>
+        <img src="images/viewers-national.png" alt="Viewrs" />
+      </Wrap>
+    </Container>
+  );
 }
 
 export default Viewers;
+
+const Container = styled.div`
+  margin-top: 30px;
+  padding: 30px 0 26px;
+  display: grid;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+  grid-gap: 20px;
+`;
+
+const Wrap = styled.div`
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  transition: 250ms all ease;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    border: 3px solid rgba(249, 249, 249, 0.8);
+  }
+`;
