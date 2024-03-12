48e2e29618ef75037d1b6f0f5f27293712ffbc82
Ashok Choudhary <ashoka8769@gmail.com>
2024-03-12 18:46:56 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Detail from "./components/Detail";
